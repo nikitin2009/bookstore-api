59cdb71305e56690fd9609a505faf9248eb7f1d7
Alex N <nikitin2009@gmail.com>
2019-07-23 15:36:11 +0300

    Fix CORS error

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,14 @@ module BookstoreApi
 
     config.api_only = true
     config.debug_exception_response_format = :default
+
+    # Fix CORS error
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
